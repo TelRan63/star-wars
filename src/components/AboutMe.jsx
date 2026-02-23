a16c8082918ddee4cf3fba68c87ae4cf0ac4704e
edward <edwardlfish@gmail.com>
2026-02-23 17:51:08 +0200

    Refactor AboutMe and Contact components to implement localStorage-based caching for API responses, and add Contact form with styling.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,25 +1,36 @@
-import {base_url} from "../utils/constants.js";
+import {base_url, period_month} from "../utils/constants.js";
 import {useEffect, useState} from "react";
 
 const AboutMe = () => {
-    const [hero, setHero] = useState();
+    const [hero, setHero] = useState(() => {
+        const hero = JSON.parse(localStorage.getItem("hero"));
+        if (hero && ((Date.now() - hero.timestamp) < period_month)) {
+            return hero.payload;
+        }
+    });
 
     useEffect(() => {
-        fetch(`${base_url}/v1/peoples/1`)
-            .then(response => response.json())
-            .then(data => {
-                const info = {
-                    name: data.name,
-                    gender: data.gender,
-                    birth_year: data.birth_year,
-                    height: data.height,
-                    mass: data.mass,
-                    hair_color: data.hair_color,
-                    skin_color: data.skin_color,
-                    eye_color: data.eye_color
-                }
-                setHero(info);
-            })
+        if (!hero) {
+            fetch(`${base_url}/v1/peoples/1`)
+                .then(response => response.json())
+                .then(data => {
+                    const info = {
+                        name: data.name,
+                        gender: data.gender,
+                        birth_year: data.birth_year,
+                        height: data.height,
+                        mass: data.mass,
+                        hair_color: data.hair_color,
+                        skin_color: data.skin_color,
+                        eye_color: data.eye_color
+                    }
+                    setHero(info);
+                    localStorage.setItem("hero", JSON.stringify({
+                        payload: info,
+                        timestamp: Date.now()
+                    }));
+                })
+        }
     }, [])
 
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,55 @@
+import '../Contact.css';
+import {base_url, period_month} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+
 const Contact = () => {
+    const [planets, setPlanets] = useState(() => {
+        const planets = JSON.parse(localStorage.getItem('planets'));
+        if (planets && ((Date.now() - planets.time) < period_month)) {
+            return planets.payload;
+        } else {
+            return ['wait...']
+        }
+    });
+
+    useEffect(() => {
+        const getPlanets = async () => {
+            const res = await fetch(`${base_url}/v1/planets`);
+            const data = await res.json();
+            const planets = data.map(item => item.name);
+            setPlanets(planets);
+            localStorage.setItem('planets', JSON.stringify({
+                payload: planets,
+                time: Date.now()
+            }));
+        }
+
+        if (planets.length === 1){
+            getPlanets().then(() => console.log('Planets were loaded'));
+        }
+    }, [])
+
     return (
-        <div>
-            Contact
-        </div>
+        <form className="container" onSubmit={e => {
+            e.preventDefault();
+        }}>
+            <label>First Name
+                <input type="text" name="firstname" placeholder="Your name.."/>
+            </label>
+            <label>Last Name
+                <input type="text" name="lastname" placeholder="Your last name.."/>
+            </label>
+            <label>Planet
+                <select name="planet">
+                    {planets.map(item => <option value={item} key={item}>{item}</option>)}
+                </select>
+            </label>
+
+            <label>Subject
+                <textarea name="subject" placeholder="Write something.."></textarea>
+            </label>
+            <button type="submit">Submit</button>
+        </form>
     )
 }
 
